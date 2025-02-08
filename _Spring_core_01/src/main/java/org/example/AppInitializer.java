da3ca0535847c2de0f451d0072864e0422ede8b4
Malik <mmalith520@gmail.com>
2025-02-08 18:24:06 +0530

    Spring core and spring been life cycle done

---
+++ _Spring_core_01/src/main/java/org/example/AppInitializer.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.example.bean.MyConnection;
+import org.example.bean.TestBean1;
+import org.example.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context
+                = new AnnotationConfigApplicationContext();
+        context.register(AppConfig.class);
+        context.refresh();
+
+        TestBean1 ref1 = context.getBean(TestBean1.class);
+        System.out.println(ref1);
+        TestBean1 ref2 = context.getBean(TestBean1.class);
+        System.out.println(ref2);
+        TestBean1 ref3 = context.getBean(TestBean1.class);
+        System.out.println(ref3);
+
+        MyConnection ref4 = context.getBean(MyConnection.class);
+        System.out.println(ref4);
+        MyConnection ref5 = context.getBean(MyConnection.class);
+        System.out.println(ref5);
+
+        context.registerShutdownHook();
+
+    }
+}
