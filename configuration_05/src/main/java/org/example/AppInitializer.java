6177c616e86f7b0d374af616ea1deef20b2758a7
Malik <mmalith520@gmail.com>
2025-02-08 18:29:55 +0530

    Spring Fullmode vs Light mode and Configuration part done

---
+++ configuration_05/src/main/java/org/example/AppInitializer.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.example.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context
+                = new AnnotationConfigApplicationContext();
+        context.register(AppConfig.class);
+        //context.register(AppConfig1.class);
+        context.refresh();
+
+
+
+        context.registerShutdownHook();
+
+    }
+}
---
+++ configuration_05/src/main/java/org/example/bean/SpringBean.java
@@ -0,0 +1,7 @@
+package org.example.bean;
+
+public class SpringBean {
+    public SpringBean() {
+        System.out.println("Spring Bean Constructor");
+    }
+}
